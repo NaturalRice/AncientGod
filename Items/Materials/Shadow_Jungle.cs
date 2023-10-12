782855a898f00ad52ae714ba627abd8742f61c8d
TXequestria <txeq@tuta.io>
2023-10-12 16:44:16 +0800

    加入了影子：森林 丛林 沙漠和合成表，加入月噬和其掉落规则  On branch main  Changes to be committed: 	modified:   AcientGod.csproj 	new file:   Items/Materials/MoonDevoration.cs 	new file:   Items/Materials/MoonDevoration.png 	new file:   Items/Materials/Shadow_Desert.cs 	renamed:    "Assets/\346\234\252\344\275\277\347\224\250\347\232\204\350\264\264\345\233\276\346\226\207\344\273\266/\345\275\261\345\255\220\357\274\232\346\262\231\346\274\240.png" -> Items/Materials/Shadow_Desert.png 	new file:   Items/Materials/Shadow_Forest.cs 	renamed:    "Assets/\346\234\252\344\275\277\347\224\250\347\232\204\350\264\264\345\233\276\346\226\207\344\273\266/\345\275\261\345\255\220\357\274\232\346\243\256\346\236\227.png" -> Items/Materials/Shadow_Forest.png 	new file:   Items/Materials/Shadow_Jungle.cs 	renamed:    "Assets/\346\234\252\344\275\277\347\224\250\347\232\204\350\264\264\345\233\276\346\226\207\344\273\266/\345\275\261\345\255\220\357\274\232\344\270\233\346\236\227.png" -> Items/Materials/Shadow_Jungle.png 	modified:   Localization/en-US_Mods.AcientGod.hjson 	modified:   Localization/zh-Hans_Mods.AcientGod.hjson 	new file:   Rules/GlobalDropRules.cs

---
+++ Items/Materials/Shadow_Jungle.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using AcientGod.Items.Materials;
+
+namespace AcientGod.Items.Materials
+{
+    internal class Shadow_Jungle : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.ResearchUnlockCount = 100;
+            Item.maxStack = 9999;
+            Item.width = 100;
+            Item.height = 100;
+        }
+        public override void AddRecipes()
+        {
+            Recipe new_recipe = CreateRecipe();
+            new_recipe.AddIngredient(ItemID.RichMahogany, 10);
+            new_recipe.AddIngredient(ItemID.MudBlock, 20);
+            new_recipe.AddIngredient(ItemID.JungleGrassSeeds, 1);
+            new_recipe.AddIngredient<MoonDevoration>(10);
+            new_recipe.AddTile(TileID.WorkBenches);
+            new_recipe.Register();
+        }
+    }
+}
