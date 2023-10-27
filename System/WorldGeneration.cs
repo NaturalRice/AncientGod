7f23edc25c9849eaabdc1ea7499fa7c125e4ea0e
TXequestria <txeq@tuta.io>
2023-10-27 17:12:20 +0800

    调整了一下环境影子的配方 找到了除以零漏洞的根源，详见Items/Tiles/BaseEnclosure.cs和Items/Tiles/BasePanel.cs里PreDraw函数的注释 尝试了世界生成代码，没有成功 尝试恢复失效的翻译，但导致了老翻译丢失，要重新加入物品命名和解说文字 On branch txeq  Changes to be committed: 	modified:   Items/Materials/Shadow_Desert.cs 	modified:   Items/Materials/Shadow_Forest.cs 	modified:   Items/Materials/Shadow_Jungle.cs 	modified:   Items/Tiles/BaseEnclosure.cs 	modified:   Items/Tiles/BasePanel.cs 	deleted:    Localization/en-US_Mods.AcientGod.hjson 	new file:   Localization/en-US_Mods.AncientGod.hjson 	deleted:    Localization/zh-Hans_Mods.AcientGod.hjson 	new file:   Localization/zh-Hans_Mods.AncientGod.hjson 	modified:   Rules/GlobalDropRules.cs 	new file:   System/WorldGeneration.cs 	deleted:    en-US.hjson 	deleted:    zh-Hans.hjson

---
+++ System/WorldGeneration.cs
@@ -0,0 +1,62 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Generation;
+using System.Collections.Generic;
+using Terraria.WorldBuilding;
+using Terraria.IO;
+using Terraria.Localization;
+
+namespace AncientGod.System
+{
+    // 2. Our world generation code must start from a class extending ModSystem
+    public class WorldGeneration : ModSystem
+    {
+        // 3. These lines setup the localization for the message shown during world generation. Update your localization files after building and reloading the mod to provide values for this.
+        public static LocalizedText WorldGenTutorialOresPassMessage { get; private set; }
+
+        public override void SetStaticDefaults()
+        {
+            WorldGenTutorialOresPassMessage = Language.GetOrRegister(Mod.GetLocalizationKey($"WorldGen.{nameof(WorldGenTutorialOresPassMessage)}"));
+        }
+
+        // 4. We use the ModifyWorldGenTasks method to tell the game the order that our world generation code should run
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight)
+        {
+            // 5. We use FindIndex to locate the index of the vanilla world generation task called "Shinies". This ensures our code runs at the correct step.
+            int ShiniesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Shinies"));
+            if (ShiniesIndex != -1)
+            {
+                // 6. We register our world generation pass by passing in an instance of our custom GenPass class below. The GenPass class will execute our world generation code.
+                tasks.Insert(ShiniesIndex + 1, new WorldGenTutorialOresPass("World Gen Tutorial Ores", 100f));
+            }
+        }
+    }
+
+    // 7. Make sure to inherit from the GenPass class.
+    public class WorldGenTutorialOresPass : GenPass
+    {
+        public WorldGenTutorialOresPass(string name, float loadWeight) : base(name, loadWeight)
+        {
+        }
+
+        // 8. The ApplyPass method is where the actual world generation code is placed.
+        protected override void ApplyPass(GenerationProgress progress, GameConfiguration configuration)
+        {
+            // 9. Setting a progress message is always a good idea. This is the message the user sees during world generation and can be useful to help users and modders identify passes that are stuck.      
+            progress.Message = WorldGeneration.WorldGenTutorialOresPassMessage.Value;
+
+            // 10. Here we use a for loop to run the code inside the loop many times. This for loop scales to the product of Main.maxTilesX, Main.maxTilesY, and 2E-05. 2E-05 is scientific notation and equal to 0.00002. Sometimes scientific notation is easier to read when dealing with a lot of zeros.
+            // 11. In a small world, this math results in 4200 * 1200 * 0.00002, which is about 100. This means that we'll run the code inside the for loop 100 times. This is the amount Crimtane or Demonite will spawn. Since we are scaling by both dimensions of the world size, the ammount spawned will adjust automatically to different world sizes for a consistent distribution of ores.
+            for (int k = 0; k < (int)((Main.maxTilesX * Main.maxTilesY) * 6E-05); k++)
+            {
+                // 12. We randomly choose an x and y coordinate. The x coordinate is choosen from the far left to the far right coordinates. The y coordinate, however, is choosen from between GenVars.worldSurfaceLow and the bottom of the map. We can use this technique to determine the depth that our ore should spawn at.
+                int x = WorldGen.genRand.Next(0, Main.maxTilesX);
+                int y = WorldGen.genRand.Next((int)GenVars.worldSurfaceLow, Main.maxTilesY);
+
+                // 13. Finally, we do the actual world generation code. In this example, we use the WorldGen.TileRunner method. This method spawns splotches of the Tile type we provide to the method. The behavior of TileRunner is detailed in the Useful Methods section below.
+                WorldGen.TileRunner(x, y, WorldGen.genRand.Next(3, 6), WorldGen.genRand.Next(2, 6), TileID.CobaltBrick);
+            }
+        }
+    }
+}
